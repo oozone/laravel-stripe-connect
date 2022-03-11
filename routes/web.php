3dd19b4e386c7f9bc088023f745c3d9f8b7fedbe
Simon Hamp <simon.hamp@me.com>
2022-03-11 15:21:24 +0000

    Improve Service Provider registration

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,18 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Response;
 
-Route::middleware('auth')
-    ->prefix('stripe-connect')
-    ->name('stripe-connect.')
-    ->group(function () {
-        Route::get('return', function () {
-            $account = Auth::user()->retrieveStripeAccount();
+Route::get('return', function () {
+    $account = Auth::user()->retrieveStripeAccount();
 
-            Auth::user()
-                ->setStripeAccountStatus($account->details_submitted)
-                ->save();
+    Auth::user()
+        ->setStripeAccountStatus($account->details_submitted)
+        ->save();
 
-            return Route::has(Config::get('stripe_connect.routes.complete'))
-                ? Response::redirectToRoute(Config::get('stripe_connect.routes.complete'))
-                : Response::redirectTo('/');
-        })->name('return');
+    return Route::has(Config::get('stripe_connect.routes.complete'))
+        ? Response::redirectToRoute(Config::get('stripe_connect.routes.complete'))
+        : Response::redirectTo('/');
+})->name('return');
 
-        Route::get('refresh', function () {
-            return Response::redirectTo(Auth::user()->getStripeAccountLink());
-        })->name('refresh');
-});
+Route::get('refresh', function () {
+    return Response::redirectTo(Auth::user()->getStripeAccountLink());
+})->name('refresh');
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,26 +2,67 @@
 
 namespace SimonHamp\LaravelStripeConnect;
 
+use Stripe\StripeClient;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use SimonHamp\LaravelStripeConnect\Interfaces\StripeConnect as StripeConnectInterface;
-use Stripe\StripeClient;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/../migrations');
-        $this->mergeConfigFrom(__DIR__.'/../config/stripe_connect.php', 'stripe_connect');
-        $this->publishes([__DIR__.'/../config', __DIR__.'/../migrations']);
+        $this->registerRoutes();
+        $this->registerMigrations();
+        $this->registerPublishing();
     }
 
     public function register()
     {
+        $this->configure();
+
         App::singleton(StripeConnectInterface::class, function () {
             return new StripeClient(Config::get('stripe_connect.stripe.secret'));
         });
+    }
+
+    protected function configure()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/stripe_connect.php.php',
+            'stripe_connect'
+        );
+    }
+
+    protected function registerRoutes()
+    {
+        Route::group([
+            'prefix' => 'stripe-connect',
+            'as' => 'stripe-connect.',
+            'middleware' => 'auth',
+        ], function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        });
+    }
+
+    protected function registerMigrations()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        }
+    }
+
+    protected function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/stripe_connect.php' => $this->app->configPath('stripe_connect.php'),
+            ], 'stripe-connect-config');
+
+            $this->publishes([
+                __DIR__.'/../database/migrations' => $this->app->databasePath('migrations'),
+            ], 'stripe-connect-migrations');
+        }
     }
 }
