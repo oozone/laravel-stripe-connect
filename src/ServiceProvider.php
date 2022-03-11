a50c7a9bddbb8d0ae0be2ce3c67de97ee76ef9f5
Simon Hamp <simon@chiaro.co.uk>
2022-03-11 04:10:38 +0000

    Bring it all together

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,31 +1,27 @@
 <?php
 
+namespace SimonHamp\LaravelStripeConnect;
 
-namespace Rap2hpoutre\LaravelStripeConnect;
-
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
-
+use SimonHamp\LaravelStripeConnect\Interfaces\StripeConnect as StripeConnectInterface;
+use Stripe\StripeClient;
 
 class ServiceProvider extends BaseServiceProvider
 {
-
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot()
     {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/../migrations');
+        $this->mergeConfigFrom(__DIR__.'/../config/stripe_connect.php', 'stripe_connect');
+        $this->publishes([__DIR__.'/../config', __DIR__.'/../migrations']);
     }
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        App::singleton(StripeConnectInterface::class, function () {
+            return new StripeClient(Config::get('stripe_connect.stripe.secret'));
+        });
     }
 }
---
+++ src/Traits/Payable.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace SimonHamp\LaravelStripeConnect\Traits;
+
+use Stripe\Account;
+use Stripe\StripeClient;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
+use SimonHamp\LaravelStripeConnect\Interfaces\StripeConnect;
+
+trait Payable
+{
+    protected static StripeClient $stripe;
+
+    protected Account $stripe_connect_account;
+
+    protected static function bootPayable()
+    {
+        static::$stripe = App::make(StripeConnect::class);
+    }
+
+    /**
+     * Create a new Stripe Connect account for this model
+     */
+    public function createStripeAccount(array $details): self
+    {
+        $this->stripe_connect_account = static::$stripe->accounts->create($details);
+
+        $this->setStripeAccountId($this->stripe_connect_account->id)->save();
+
+        return $this;
+    }
+
+    /**
+     * Get the latest details about this account from Stripe
+     */
+    public function retrieveStripeAccount(): Account
+    {
+        return $this->stripe_connect_account = static::$stripe->accounts->retrieve($this->getStripeAccountId());
+    }
+
+    public function getStripeAccountId()
+    {
+        return $this->{$this->getStripeAccountIdColumn()};
+    }
+
+    public function isStripeAccountActive()
+    {
+        return $this->{$this->getStripeAccountStatusColumn()};
+    }
+
+    /**
+     * Get the redirect URL needed to take this account through Stripe's onboarding flow
+     */
+    public function getStripeAccountLink($type = 'account_onboarding'): string
+    {
+        $link = static::$stripe->accountLinks->create(
+            [
+                'account' => $this->getStripeAccountId(),
+                'refresh_url' => URL::route(Config::get('stripe_connect.routes.account.refresh')),
+                'return_url' => URL::route(Config::get('stripe_connect.routes.account.return')),
+                'type' => 'account_onboarding',
+            ]
+        );
+
+        return $link->url;
+    }
+
+    public function pay($amount, $currency)
+    {
+        return static::$stripe->transfers->create([
+            'amount' => $amount,
+            'currency' => $currency,
+            'destination' => $this->getStripeAccountId(),
+        ]);
+    }
+
+    protected function getStripeAccountIdColumn()
+    {
+        return Config::get('stripe_connect.payable.account_id_column');
+    }
+
+    protected function setStripeAccountId($id)
+    {
+        $this->{$this->getStripeAccountIdColumn()} = $id;
+
+        return $this;
+    }
+
+    protected function getStripeAccountStatusColumn()
+    {
+        return Config::get('stripe_connect.payable.account_status_column');
+    }
+
+    protected function setStripeAccountStatus($status)
+    {
+        $this->{$this->getStripeAccountStatusColumn()} = $status;
+
+        return $this;
+    }
+}
